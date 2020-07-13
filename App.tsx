fccf36213db6ac322f8a6a24d2cea654558276ef
Guy Avraham <guy1812@gmail.com>
2020-07-13 14:16:40 +0300

    saga error

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { StyleSheet, View, Button } from "react-native";
-import { Provider } from "react-redux";
+import {StyleSheet, View, Button} from "react-native";
+import {Provider} from "react-redux";
+import * as Sentry from 'sentry-expo';
 
 import store from "./state";
-import { makeErrorAsyncAction } from "./state/actions";
+import {makeErrorAsyncAction} from "./state/actions";
+
+Sentry.init({
+  dsn: 'DONT UPLOAD SECRETS TO GIT',
+  enableInExpoDevelopment: true,
+  debug: true,
+});
 
 export default function App() {
-  const onPress = () => {
-    // store.dispatch(makeErrorAsyncAction());
-    throw new Error("FFF");
-  };
 
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Button title="Do error" onPress={onPress} />
+        <Button title="throw error from button" onPress={() => {
+          throw new Error("error from button")
+        }}/>
+
+        <Button title="throw error redux action (saga)" onPress={() => {
+          store.dispatch(makeErrorAsyncAction());
+        }}/>
       </View>
     </Provider>
   );
--- state/saga.ts
+++ state/saga.ts
@@ -5,7 +5,8 @@ import { MAKE_ERROR_ASYNC_ACTION } from "./actions";
 function* makeError() {
   console.log("make error saga");
   yield delay(2000);
-  //   throw "ERRR";
+  // throw Error('error from saga action');
+  throw 'error from saga action';
 }
 
 export default function* rootSaga() {
